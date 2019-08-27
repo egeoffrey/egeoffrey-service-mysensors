3d906316d65c1a1d3080bf7c0bc99e43967a1ed6
user2684 <user2684@users.noreply.github.com>
2019-08-27 19:13:33 +0200

    Added module_configuration to manifest file

--- service/mysensors_mqtt.py
+++ service/mysensors_mqtt.py
@@ -69,7 +69,8 @@ class Mysensors_mqtt(Mysensors):
         self.gateway = mqtt.Client()
         try: 
             self.log_info("Connecting to MQTT gateway on "+self.config["hostname"]+":"+str(self.config["port"]))
-            if "username" in self.config and "password" in self.config: self.gateway.username_pw_set(self.config["username"], password=self.config["password"])
+            password = self.config["password"] if "password" in self.config else ""
+            if "username" in self.config: self.gateway.username_pw_set(self.config["username"], password=password)
             self.gateway.connect(self.config["hostname"], self.config["port"], 60)
         except Exception,e:
             self.log_warning("Unable to connect to the MQTT gateway: "+exception.get(e))
